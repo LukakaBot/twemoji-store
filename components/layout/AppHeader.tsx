9759fb1ed1dfcbb1226f7e1cba9f07b4bf82cd77
鲨鱼辣椒 <monishine_code@163.com>
2025-08-02 19:40:26 +0800

    feat: AppHeader

--- components/layout/AppHeader.tsx
+++ components/layout/AppHeader.tsx
@@ -1,15 +1,30 @@
 import AppIcon from "@/components/base/AppIcon";
 
 function AppHeader() {
-	return 		<header className='border-b-border sticky top-0 z-30 border-b border-dashed'>
-			<div className='container-wrapper flex justify-between items-center gap-2'>
-				<div className='flex-1 flex gap-2 items-center'>
-					<AppIcon className='text-red-500' name='icon-[bxs--hot]' />
-					<div>每日热搜</div>
-				</div>
-				
-			</div>
-		</header>;
+  return (
+    <header className="border-b-border sticky top-0 z-30 border-b border-dashed">
+      <div className="container-wrapper flex justify-between items-center gap-2 text-base font-semibold">
+        <div className="flex-1 flex gap-2 items-center">
+          <AppIcon name="svg-partying-face" />
+          {/* <div>🥳</div> */}
+          <p>Twemoji Store</p>
+        </div>
+        <div>
+          <p>
+            Emojis by Twitter's{" "}
+            <a
+              className="text-[#ffcc4d] hover:border-solid hover:border-b-2 border-[#ffcc4d]"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://github.com/twitter/twemoji"
+            >
+              Twemoji
+            </a>
+          </p>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default AppHeader;
