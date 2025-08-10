66296e5c9efdc56b363d030efc3eb7cdfdce597d
鲨鱼辣椒 <monishine_code@163.com>
2025-08-10 22:08:08 +0800

    feat: 绘制emoji卡片

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
+import Image from 'next/image';
+import emojis from 'emojibase-data/zh/data.json'
+
+function getEmoji(name: string) {
+  const lowerName = name.toLowerCase()
+  return `https://cdn.bootcdn.net/ajax/libs/twemoji/16.0.1/svg/${lowerName}.svg`
+}
+
 export default function Home() {
   return (
     <div className="min-h-screen container-wrapper">
       <main className="">
         <div className="grid grid-cols-20 gap-2">
-          {Array.from({ length: 20 }).map((item, index) => (
+          {emojis.map((emoji) => (
             <div
-              key={index}
-              className="flex justify-center items-center p-4 rounded-2xl bg-[#F4F5F5] text-center cursor-pointer text-2xl hover:scale-105 transition-all duration-300  outline-2 outline-transparent hover:outline-[#ffcc4d]"
+              key={emoji.emoji}
+              className="flex justify-center items-center p-4 rounded-2xl bg-[#F4F5F5] text-center cursor-pointer text-3xl hover:scale-110 box-border outline-2 outline-solid outline-transparent hover:outline-[#ffcc4d]"
             >
-              🥳
+              <Image src={getEmoji(emoji.hexcode)} width={24} height={24} alt={emoji.emoji} loading="lazy" />
             </div>
           ))}
         </div>
