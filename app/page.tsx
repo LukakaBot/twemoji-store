f1ed110f7c2c0937e3b5983780745ca35d6d8646
鲨鱼辣椒 <monishine_code@163.com>
2025-08-11 14:03:04 +0800

    style: emoji卡片添加动画效果

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="grid grid-cols-20 gap-2">
           {emojis.map((emoji) => (
             <div
-              className="flex justify-center items-center p-4 rounded-[30%] bg-[#F4F5F5] text-center cursor-pointer text-3xl hover:scale-110 box-border outline-2 outline-solid outline-transparent hover:outline-[#ffcc4d]"
+              className="flex justify-center items-center p-4 rounded-[30%] bg-[#F4F5F5] text-center cursor-pointer text-3xl hover:scale-110 box-border transition-all duration-300 ease-in-out outline-2 outline-solid outline-transparent hover:outline-[#ffcc4d]"
               key={emoji.emoji}
             >
               <Image loader={imageLoader} src={emoji.hexcode} width={32} height={32} alt={emoji.emoji} loading="lazy" />
