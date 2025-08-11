a7fcd2231178881cdb9e724a967f7c4a53dc2563
鲨鱼辣椒 <monishine_code@163.com>
2025-08-11 16:52:18 +0800

    style: 调整emoji卡片样式

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="min-h-screen container-wrapper">
       <main>
-        <div className="grid grid-cols-20 gap-2">
+        <div className="grid grid-cols-20 gap-3">
           {emojis.map((emoji) => (
             <div
               className="flex justify-center items-center p-4 rounded-[30%] bg-[var(--layout)] text-center cursor-pointer text-3xl hover:scale-110 box-border transition-all duration-300 ease-in-out outline-2 outline-solid outline-transparent hover:outline-[#ffcc4d]"
---
+++ components/base/AppSearch.tsx
@@ -0,0 +1,11 @@
+import groupsSubgroups from 'emojibase-data/zh/messages.json';
+
+export default function AppSearch() {
+  console.log(groupsSubgroups)
+  const skinTones = []
+
+
+  return (
+    <div className='mb-4'>search</div>
+  )
+}
