2c713de22409397726efb7ec4e9e19d823f6eda1
鲨鱼辣椒 <monishine_code@163.com>
2025-08-13 17:14:30 +0800

    feat(utils): 修改获取emoji图片地址函数，增加获取皮肤emoji函数

--- utils/tool/index.ts
+++ utils/tool/index.ts
@@ -1,15 +1,17 @@
 import type { Emoji } from 'emojibase'
-import type { ImageLoaderProps } from 'next/image'
 
-export function getEmojiUrl(emoji: Emoji, type: 'svg' | 'png' = 'svg', skinTone?: string) {
+export function findSkinEmoji(emoji: Emoji, skinTone: string) {
+  if (skinTone && skinTone !== 'default' && emoji?.skins) {
+    const reg = new RegExp(skinTone, 'i')
+    return emoji.skins.filter(skin => skin.hexcode.search(reg) !== -1)[0]
+  }
+  return emoji
+}
+
+export function getEmojiUrl(emoji: Emoji, type: 'svg' | 'png' = 'svg') {
   const CDN_BASE_URL = 'https://cdn.bootcdn.net/ajax/libs/twemoji/16.0.1'
   let folder = 'svg'
   let code = emoji.hexcode.toLowerCase()
-
-  if (skinTone && emoji?.skins) {
-    const reg = new RegExp(skinTone, 'i')
-    code = emoji.skins.filter(skin => skin.hexcode.search(reg) !== -1)[0].hexcode.toLowerCase()
-  }
 
   if (type === 'png') {
     folder = '72x72'
@@ -24,19 +26,4 @@ export function getEmojiUrl(emoji: Emoji, type: 'svg' | 'png' = 'svg', skinTone?
   }
 
   return `${CDN_BASE_URL}/${folder}/${code}.${type}`
-}
-
-export function imageLoader({ src }: ImageLoaderProps) {
-  const CDN_BASE_URL = 'https://cdn.bootcdn.net/ajax/libs/twemoji/16.0.1/svg'
-  let code = src.toLowerCase()
-
-  if (code.startsWith('00')) {
-    code = code.slice(2).replace(/-fe0f/g, '')
-  }
-
-  if (code === '1f441-fe0f-200d-1f5e8-fe0f') {
-    code = code.replace(/-fe0f/g, '')
-  }
-
-  return `${CDN_BASE_URL}/${code}.svg`
 }
