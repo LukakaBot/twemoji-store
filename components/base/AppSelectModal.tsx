9463723a5b3fe58984485eaec07217e0ff7c8564
鲨鱼辣椒 <monishine_code@163.com>
2025-08-13 14:13:43 +0800

    style: 调整emoji弹窗样式

--- components/base/AppSelectModal.tsx
+++ components/base/AppSelectModal.tsx
@@ -1,12 +1,11 @@
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
 import Image from 'next/image'
-import { useImperativeHandle, useState } from 'react'
+import { useImperativeHandle, useMemo, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
@@ -36,6 +35,8 @@ export default function AppSelectModal(props: AppSelectModalProps) {
   const [open, setOpen] = useState(false)
   const { selectEmoji, skinTone, setSkinTone } = useConfigStore()
 
+  const emojiUrl = useMemo(() => selectEmoji ? getEmojiUrl(selectEmoji, 'svg', skinTone) : '', [selectEmoji, skinTone])
+
   const openModal = () => {
     setOpen(true)
   }
@@ -52,38 +53,64 @@ export default function AppSelectModal(props: AppSelectModalProps) {
   return (
     selectEmoji && (
       <Dialog open={open} onOpenChange={setOpen}>
-        <form>
-          <DialogContent className="sm:max-w-[425px]">
+        <DialogContent className="sm:max-w-[400px]">
+          <VisuallyHidden>
             <DialogHeader>
-              <Image className="mb-4" src={getEmojiUrl(selectEmoji, 'svg', skinTone)} width={90} height={90} alt={selectEmoji?.emoji} loading="lazy" />
-              <DialogTitle className="text-left">{selectEmoji.label}</DialogTitle>
-              <DialogDescription className="text-left">
-                [
-                {selectEmoji?.tags && selectEmoji.tags?.map(tag => `#${tag}`).join(' ')}
-                ]
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
               </DialogDescription>
             </DialogHeader>
-            {selectEmoji?.skins && (
-              <div>
-                <ToggleGroup value={skinTone} variant="outline" type="single" onValueChange={setSkinTone}>
-                  {
-                    skinTones.map(skinTone => (
-                      <ToggleGroupItem value={skinTone.value} aria-label={skinTone.ariaLabel} key={skinTone.ariaLabel}>
-                        <div className={`w-3 h-3 rounded-full ${skinTone.bg}`} />
-                      </ToggleGroupItem>
-                    ))
-                  }
-                </ToggleGroup>
+          </VisuallyHidden>
+          <div className="flex gap-4">
+            <Image className="mb-4" src={emojiUrl} width={90} height={90} alt={selectEmoji?.emoji} loading="lazy" />
+            <div className="flex flex-col gap-2">
+              <div className="text-lg leading-none font-semibold text-left">{selectEmoji.label}</div>
+              <div className="text-muted-foreground text-sm">
+                <div className="flex gap-2">
+                  <span className="w-18 font-semibold">tag:</span>
+                  <span className="flex-1">
+                    [
+                    {selectEmoji?.tags && selectEmoji.tags?.map(tag => `#${tag}`).join(' ')}
+                    ]
+                  </span>
+                </div>
+                <div className="flex gap-2">
+                  <span className="w-18 font-semibold">emoji:</span>
+                  <span className="flex-1">{selectEmoji.emoji}</span>
+                </div>
+                <div className="flex gap-2">
+                  <span className="w-18 font-semibold">since:</span>
+                  <span className="flex-1">{`version ${selectEmoji.version}`}</span>
+                </div>
+                <div className="flex gap-2">
+                  <span className="w-18 font-semibold">hex:</span>
+                  <span className="flex-1">{selectEmoji.hexcode.toUpperCase()}</span>
+                </div>
               </div>
-            )}
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DialogClose>
-              <Button type="submit">Save changes</Button>
-            </DialogFooter>
-          </DialogContent>
-        </form>
+            </div>
+          </div>
+          {selectEmoji?.skins && (
+            <div>
+              <span className="my-1 text-sm">Skin Tone</span>
+              <ToggleGroup value={skinTone} variant="outline" type="single" onValueChange={setSkinTone}>
+                {
+                  skinTones.map(skinTone => (
+                    <ToggleGroupItem value={skinTone.value} aria-label={skinTone.ariaLabel} key={skinTone.ariaLabel}>
+                      <div className={`w-3 h-3 rounded-full ${skinTone.bg}`} />
+                    </ToggleGroupItem>
+                  ))
+                }
+              </ToggleGroup>
+            </div>
+          )}
+          <div className="grid grid-cols-3 gap-2">
+            <Button className="bg-mustard-500 hover:bg-mustard-300">Copy</Button>
+            <Button className="bg-mustard-500 hover:bg-mustard-300">svg</Button>
+            <Button className="bg-mustard-500 hover:bg-mustard-300">png</Button>
+          </div>
+        </DialogContent>
       </Dialog>
     )
   )
