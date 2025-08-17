80cd5598badd435c2c2f0f7f37bcc1c211468e68
鲨鱼辣椒 <monishine_code@163.com>
2025-08-17 18:16:18 +0800

    feat: 调整emoji卡片grid布局

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,8 @@ export default function Home() {
   return (
     <div className="min-h-screen container-wrapper">
       <main>
-        <div className="grid grid-cols-20 gap-3">
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(60px,1fr))] gap-3">
+
           {emojis.map(emoji => (
             <div
               className="flex justify-center items-center p-4 rounded-[30%] bg-[var(--layout)] text-center cursor-pointer text-3xl hover:scale-110 box-border transition-all duration-300 ease-in-out outline-2 outline-solid outline-transparent hover:outline-[#ffcc4d]"
